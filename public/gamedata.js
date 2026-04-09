3cd84111f9180d4d5c008c0cd26d433347185b4a
foymiaretyzja-netizen <foymiaretyzja@gmail.com>
2026-04-09 09:20:37 -0500

    Update gamedata.js

--- public/gamedata.js
+++ public/gamedata.js
@@ -1,27 +1,9 @@
 const gamesLibrary = [
     {
-        title: "Smash Karts",
-        image: "https://images.crazygames.com/smash-karts/cover-1583232506253.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop",
-        url: "https://smashkarts.io/",
+        title: "Geometry Dash Lite",
+        image: "GameCovers/geometrydash.png", 
+        url: "geometrydash.html", 
         featured: true
-    },
-    {
-        title: "1v1.LOL",
-        image: "https://images.crazygames.com/1v1-lol/cover-1594911266224.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop",
-        url: "https://1v1.lol/",
-        featured: true
-    },
-    {
-        title: "Retro Bowl",
-        image: "https://images.crazygames.com/games/retro-bowl/cover-1644917088924.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop",
-        url: "https://retrobowl.me/",
-        featured: false
-    },
-    {
-        title: "Monkey Mart",
-        image: "https://images.crazygames.com/monkey-mart/cover-1675266155940.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop",
-        url: "https://monkeymart.org/",
-        featured: false
     }
-    // Add as many as you want here!
+    // Just drop your next games right below this one!
 ];
